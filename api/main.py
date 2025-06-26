1a9152abfa6bce22c165c42d5e455c76f7c9866f
Guilherme Siani <business.guilhermesiani@gmail.com>
2025-06-26 17:08:01 -0300

    mod api/main.py
    date definition moved outside of for loop, time column added to the simulated data, transaction_id removed (will be added back as an autoincremental column in the future)

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-from datetime import date
+from datetime import date, time
 import random
-import uuid
 
 categories = ['Electronics', 'Clothing', 'Tools', 'Food', 'Toys']
 statuses = ['Pending', 'Shipped', 'Delivered', 'Cancelled']
@@ -13,31 +12,40 @@ def get_data():
 
     data = []
 
+    # today's date
+    today = date.today().isoformat()
+
     # random ammount of records
-    for _ in range(random.randint(100, 500)):
+    num_records = random.randint(1000, 5000)
+    for _ in range(num_records):
 
         # completely random data
-        transaction_id = str(uuid.uuid4())
         seller_id = random.randint(1, 50)
         product_id = random.randint(1, 100)
         quantity = random.randint(1, 10)
         status = random.choice(statuses)
+        purchase_time = time(
+            hour=random.randint(0, 23),
+            minute=random.randint(0, 59),
+            second=random.randint(0, 59)
+        ).isoformat()
 
-        product_rng = random.Random(product_id)  # rng seeded on product_id for consistent prices and categories
+        # rng seeded on product_id for consistent prices and categories
+        product_rng = random.Random(product_id)
 
         # product-consistent random data
         category = product_rng.choice(categories)
         price = round(product_rng.uniform(19.99, 1499.99), 2)
 
         record = {
-            'transaction_id': transaction_id,
             'seller_id': seller_id,
             'product_id': product_id,
             'category': category,
             'quantity': quantity,
             'price': price,
             'status': status,
-            'date': date.today()
+            'date': today,
+            'time': purchase_time
         }
         data.append(record)
 
